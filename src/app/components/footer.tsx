9674436cb04e235ef01b7e71259c5e6873beede1
krishna <krishnacoder20@gmail.com>
2024-06-02 21:24:03 +0530

    Update files

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function footer() {
+     return (
+          <div className="w-full p-2 bg-blue-600 mt-10 text-center">
+               Compyright © Boat - All rights reserved! made by krishna ❤️
+          </div>
+     );
+}
---
+++ src/app/components/header.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+
+
+export default function Header() {
+  return (
+    <nav className="w-full h-[50px] bg-blue-500 p-5 flex items-center justify-between mb-20">
+      <div className="logo text-2xl flex items-center">
+        <span className="text-white">&lt;</span>
+        <p className="text-white">Bo</p>
+        <span className="text-black font-bold">at /&gt;</span>
+      </div>
+      <div className="w-fit p-2">
+        <button className="w-[130px] h-[40px] flex items-center gap-2 font-bold p-3 bg-black text-white justify-center rounded-full hover:bg-transparent hover:border-2 border-black text-1xl">
+            <Image className="GithubLogo" width={24} height={24} src="/github.svg" alt="icons" />
+          Github
+        </button>
+      </div>
+    </nav>
+  );
+}
